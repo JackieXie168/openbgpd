d3b229ba454482a73934c134920a465bcfe319fc
Peter van Dijk <peter.van.dijk@powerdns.com>
2016-12-29 19:01:02 +0100

    show neighbor marked down in bgpctl show neighbor

--- bgpctl/bgpctl.c
+++ bgpctl/bgpctl.c
@@ -723,6 +723,7 @@ show_neighbor_msg(struct imsg *imsg, enum neighbor_views nv)
 		    inet_ntoa(ina));
 		printf("%s\n", print_auth_method(p->auth.method));
 		printf("  BGP state = %s", statenames[p->state]);
+		if(p->conf.down) printf(", marked down");
 		if (p->stats.last_updown != 0)
 			printf(", %s for %s",
 			    p->state == STATE_ESTABLISHED ? "up" : "down",
